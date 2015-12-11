9f2abb86e158b4babd4ecd8075eb2d47bfdd02d1
Sam Hammer <sam@michaelrobertson.com>
2015-12-11 16:07:26 -0700

    create bucketlist controller

--- app/controllers/bucketlist_controller.rb
+++ app/controllers/bucketlist_controller.rb
@@ -1,12 +1,26 @@
 class BucketlistController < ApplicationController
+  def new
+    @bucketlist = Bucketlist.new
+  end
+
+  def create
+    @bucketlist = Bucketlist.find(params[:id])
+  end
+
   def show
+    @bucketlist = Bucketlist.find(params[:id])
+    @profile = current_user.profile
   end
 
   def edit
+    @bucketlist = Bucketlist.find(params[:id])
+    @profile = Bucketlist.profile(:id)
+    @bucketlist.update(bucketlist_params)
   end
 
-  def new
-  end
+  def update
+
+
 
   def index
   end
